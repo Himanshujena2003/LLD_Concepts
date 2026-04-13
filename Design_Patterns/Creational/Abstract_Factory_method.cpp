4ac83b807c00ebe221cfaa804b31ff939ec313ff
Himanshujena2003 <himanshujena2003@gmail.com>
2026-04-13 15:05:07 +0530

    Abstract_Factory

---
+++ Design_Patterns/Creational/Abstract_Factory_method.cpp
@@ -0,0 +1,11 @@
+// Below code is a example of Abstract Factory Method
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// Abstract Burger Class
+class Burger{
+    virtual void prepare()=0;
+    virtual ~Burger(){};
+};
+
