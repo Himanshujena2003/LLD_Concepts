439edda80b7089efc07364306e23f84f9663204d
Himanshujena2003 <himanshujena2003@gmail.com>
2026-06-10 16:45:04 +0530

    new app

---
+++ Practice_Questions/Zomato/models/MenuItem.h
@@ -0,0 +1,8 @@
+#ifndef MENUITEM_H
+#define MENUITEM_H
+
+class MenuItem{
+
+};
+
+#endif
---
+++ Practice_Questions/Zomato/models/Restaurant.h
@@ -0,0 +1,24 @@
+#ifndef RESTAURANT.H
+#define RESTAURANT.H
+
+#include<bits/stdc++.h>
+#include "MenuItem.h"
+using namespace std;
+
+class Restaurant{
+    private:
+        static int nextRestaurantId;
+        int restaurantId;
+        string name;
+        string location;
+        vector<MenuItem>menu;
+    
+    public:
+        
+
+
+
+
+};
+
+#endif
